bde067c7e8cae2406493a2c65c22ffafb7602db7
Corbin McCalister <mccalisterc2003@gmail.com>
2024-11-20 09:58:03 -0600

    Added More Info To LoanDetail

--- react/my-app/src/pages/LoanDetail.js
+++ react/my-app/src/pages/LoanDetail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'; // Import useParams to access URL parameters
 import "./Loan.css";
+import "./LoanDetail.css"
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
@@ -30,18 +31,32 @@ function LoanDetail() {
     }
 
     return (
-        <Card>
-        <Card.Header>
-            <strong>Loan Details</strong>
-        </Card.Header>
-        <ListGroup variant='flush'>
-            <ListGroup.Item><strong>Loan ID:</strong> {loan.loanId}</ListGroup.Item>
-            <ListGroup.Item><strong>Created At:</strong> {new Date(loan.created_at).toLocaleDateString()}</ListGroup.Item>
-            <ListGroup.Item><strong>Remaining Balance:</strong> {loan.loan_current_amount}</ListGroup.Item>
-            <ListGroup.Item><strong>Principle: </strong>{loan.loan_origin_amount}</ListGroup.Item>
-            <ListGroup.Item><strong>Interest Rate:</strong> {loan.interest_rate}</ListGroup.Item>
-        </ListGroup>
-        </Card>
+        <div id="card-parent">
+            <Card id="card">
+                <Card.Header>
+                    <strong>Loan Details</strong>
+                </Card.Header>
+                <ListGroup variant='flush'>
+                    <ListGroup.Item><strong>Loan ID:</strong> {loan.loanId}</ListGroup.Item>
+                    <ListGroup.Item><strong>Created At:</strong> {new Date(loan.created_at).toLocaleDateString()}</ListGroup.Item>
+                    <ListGroup.Item><strong>Remaining Balance:</strong> {loan.loan_current_amount}</ListGroup.Item>
+                    <ListGroup.Item><strong>Principle: </strong>{loan.loan_origin_amount}</ListGroup.Item>
+                    <ListGroup.Item><strong>Interest Rate:</strong> {loan.interest_rate}</ListGroup.Item>
+                </ListGroup>
+            </Card>
+            <Card id="card">
+                <Card.Header>
+                    <strong>Customer Details</strong>
+                </Card.Header>
+                <ListGroup variant='flush'>
+                    <ListGroup.Item><strong>Customer Name:</strong> {loan.userAccount.name}</ListGroup.Item>
+                    <ListGroup.Item><strong>Customer Email:</strong> {loan.userAccount.email}</ListGroup.Item>
+                    <ListGroup.Item><strong>Customer Phone Number:</strong> {loan.userAccount.phone_number}</ListGroup.Item>
+                    <ListGroup.Item><strong>Customer Bank Routing Number:</strong> {loan.userAccount.bank_routing}</ListGroup.Item>
+                    <ListGroup.Item><strong>Customer Bank Account Number:</strong> {loan.userAccount.bank_account_number}</ListGroup.Item>
+                </ListGroup>
+            </Card>
+        </div>
     );
 }
 
